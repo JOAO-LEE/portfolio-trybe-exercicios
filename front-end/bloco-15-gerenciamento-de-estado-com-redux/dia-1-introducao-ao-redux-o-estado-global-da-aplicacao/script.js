ce985cb40aa7c4becebe407f7acb034c02544aa1
João Lee <joaumlimaum@gmail.com>
2022-07-02 17:08:54 -0300

    Criando lógica no reducer

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/script.js
@@ -3,6 +3,21 @@ const INITIAL_STATE = {
   index: 0,
 };
 
-const reducer = (state = INITIAL_STATE, action) => ( state )
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length ? 0 : state.index + 1
+      }
+      case 'PREVIOUS_COLOR':
+        return {
+          ...state,
+        index: state.index === state.colors.length ? 0 : state.index - 1
+      }
+      default:
+        state;
+      }
+    }
 
 const store = Redux.createStore(reducer);
