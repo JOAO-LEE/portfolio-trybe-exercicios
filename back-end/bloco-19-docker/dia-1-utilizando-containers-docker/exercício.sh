ddd7d8c4a2eace2695665a88b04818411c71f8eb
João Lee <joaumlimaum@gmail.com>
2022-08-26 21:11:24 -0300

    Atividade 3

--- back-end/bloco-19-docker/dia-1-utilizando-containers-docker/exercício.sh
+++ back-end/bloco-19-docker/dia-1-utilizando-containers-docker/exercício.sh
@@ -2,7 +2,10 @@
 
 $docker pull debian:stable-slim;
 
-
 # 🚀 Após baixar a imagem para seu computador local, crie e execute um container no modo interativo utilizando essa imagem como referência — não esqueça referenciar a tag.
 
 $docker container run -it debian:stable-slim;
+
+# 🚀 No terminal, você deve conseguir rodar o comando cat /etc/*-release, que vai retornar os dados da distribuição Debian que está sendo rodada dentro do container.
+
+$cat /etc/*-release;
