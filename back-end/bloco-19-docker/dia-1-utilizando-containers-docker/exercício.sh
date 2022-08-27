858b857e7b2ec7f9c4c0e0d55b314e72f043c824
João Lee <joaumlimaum@gmail.com>
2022-08-26 21:13:12 -0300

    Atividade 4

--- back-end/bloco-19-docker/dia-1-utilizando-containers-docker/exercício.sh
+++ back-end/bloco-19-docker/dia-1-utilizando-containers-docker/exercício.sh
@@ -9,3 +9,20 @@ $docker container run -it debian:stable-slim;
 # 🚀 No terminal, você deve conseguir rodar o comando cat /etc/*-release, que vai retornar os dados da distribuição Debian que está sendo rodada dentro do container.
 
 $cat /etc/*-release;
+
+# Resultado mostrado:
+
+#PRETTY_NAME="Debian GNU/Linux 11 (bullseye)"
+#NAME="Debian GNU/Linux"
+#VERSION_ID="11"
+#VERSION="11 (bullseye)"
+#VERSION_CODENAME=bullseye
+#ID=debian
+#HOME_URL="https://www.debian.org/"
+#SUPPORT_URL="https://www.debian.org/support"
+#BUG_REPORT_URL="https://bugs.debian.org/"
+
+# 🚀 Encerre o terminal.
+
+$exit
+
