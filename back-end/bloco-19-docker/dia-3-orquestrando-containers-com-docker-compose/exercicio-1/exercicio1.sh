f338ee15377cd1e575728cd7b8c5f856ae5e790a
João Lee <joaumlimaum@gmail.com>
2022-08-31 01:39:59 -0300

    Atividade 2

---
+++ back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercicio-1/exercicio1.sh
@@ -0,0 +1,2 @@
+#2 - Crie um container para manter um servidor httpd:2.4 Apache e vincule sua porta interna com a porta 4545 da sua máquina local.
+$docker container run -itd --name my-server -p 4545:80 -v $(pwd)/src/:/usr/local/apache2/htdocs/ httpd:2.4
