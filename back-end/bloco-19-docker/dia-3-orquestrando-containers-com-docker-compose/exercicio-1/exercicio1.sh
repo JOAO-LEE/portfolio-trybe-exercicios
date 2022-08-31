f487d387db048adb855be5aaf8223ba84be0681a
João Lee <joaumlimaum@gmail.com>
2022-08-31 01:58:30 -0300

    Renomeia atividade 3 para 5, adiciona atividade 6

--- back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercicio-1/exercicio1.sh
+++ back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercicio-1/exercicio1.sh
@@ -1,5 +1,8 @@
 #2 - Crie um container para manter um servidor httpd:2.4 Apache e vincule sua porta interna com a porta 4545 da sua máquina local.
 $docker container run -itd --name my-server -p 4545:80 -v $(pwd)/src/:/usr/local/apache2/htdocs/ httpd:2.4
 
-# 3 - Obtenha o id do container httpd:2.4.
+# 5 - Obtenha o id do container httpd:2.4.
 $docker container ps -a 
+
+# 6 - Obtenha o Mounts através da propriedade Source, que deve mostrar o volume desse container no Docker Host;
+$docker inspect my-server
