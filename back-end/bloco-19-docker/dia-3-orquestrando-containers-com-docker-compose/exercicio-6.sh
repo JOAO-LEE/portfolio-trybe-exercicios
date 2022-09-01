4fb807b8391411552caf6cc0b8008a0a295d250f
João Lee <joaumlimaum@gmail.com>
2022-09-01 19:44:09 -0300

    Atividade 6

---
+++ back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercicio-6.sh
@@ -0,0 +1,2 @@
+# Para simularmos o processo de desenvolvimento, faça a alteração em alguma parte do código do app react, e então execute o comando para subir o serviço novamente, "rebuildando" a imagem para aplicar as alterações.
+$docker-compose up --build -d
