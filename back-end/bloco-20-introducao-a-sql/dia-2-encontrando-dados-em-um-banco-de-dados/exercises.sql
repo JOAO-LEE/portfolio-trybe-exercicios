35f3292e23bf3380b39dbb9c64c26eb85b2985b3
João Lee <joaumlimaum@gmail.com>
2022-09-05 23:43:05 -0300

    Atividade 12

--- back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
@@ -33,3 +33,7 @@ ORDER by hours DESC LIMIT 3;
 
 /* 11 - Escreva uma query para exibir o código de todos os projetos da tabela AssignedTo sem que haja repetições.*/
 SELECT DISTINCT project FROM AssignedTo;
+
+/* 12 - Escreva uma query para exibir o nome do projeto com maior quantidade de horas.*/
+SELECT name FROM Projects
+ORDER BY hours DESC LIMIT 1;
