68f082ffe3e77f147a8c13f24ff21331818216a5
João Lee <joaumlimaum@gmail.com>
2022-09-05 22:42:04 -0300

    Atividade 2

--- back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
@@ -1,2 +1,5 @@
-/* 1 - Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution". */
+/* 1 - Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".*/
 SELECT 'This is SQL Exercise, Practice and Solution';
+
+/* 2 - Escreva uma query para exibir três números em três colunas.*/
+SELECT 2 AS 'first_number', 4 AS 'second_number', 7 AS 'third_number';
