77bc7bdb24f8cd8805ff1845628777e4b821085c
João Lee <joaumlimaum@gmail.com>
2022-09-06 16:56:12 -0300

    Atividade 3

--- back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercicio-bonus/exercise-2.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercicio-bonus/exercise-2.sql
@@ -1,7 +1,12 @@
-/* 1 - Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT. */
+/* 1 - Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT.*/
 SELECT * FROM Provides
 WHERE provider = 'RBT';
 
-/* 2 - Escreve uma query para exibir todas as informações das cinco peças com os maiores preços. */
+/* 2 - Escreve uma query para exibir todas as informações das cinco peças com os maiores preços.*/
 SELECT * FROM Provides
 ORDER BY price DESC LIMIT 5;
+
+/* 3 - Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.*/
+SELECT provider, price FROM Provides
+ORDER BY price DESC LIMIT 4 OFFSET 2;
+
