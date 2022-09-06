8731aafcf373dd17ef224e438907efd09c0463c4
João Lee <joaumlimaum@gmail.com>
2022-09-05 23:03:29 -0300

    Atividade 7

--- back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
@@ -15,3 +15,7 @@ SELECT * FROM Scientists;
 
 /* 6 - Escreva uma query para exibir o nome como "nome_do_projeto" e as horas como "tempo_de_trabalho" de cada projeto. */
 SELECT name AS nome_do_projeto, hours AS tempo_de_trabalho FROM Projects;
+
+/* 7 - Escreva uma query para exibir o nome dos cientistas em ordem alfabética. */
+SELECT * FROM Scientists
+ORDER BY name;
