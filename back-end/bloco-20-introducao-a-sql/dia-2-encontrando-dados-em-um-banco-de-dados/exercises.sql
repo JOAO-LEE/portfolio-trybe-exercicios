69400b904f243036b86fff972ce176ebf572c9c7
João Lee <joaumlimaum@gmail.com>
2022-09-05 23:14:58 -0300

    Atividade 8

--- back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercises.sql
@@ -4,18 +4,22 @@ SELECT 'This is SQL Exercise, Practice and Solution';
 /* 2 - Escreva uma query para exibir três números em três colunas.*/
 SELECT 2 AS 'first_number', 4 AS 'second_number', 7 AS 'third_number';
 
-/* 3 - Escreva uma query para exibir a soma dos números 10 e 15. */
+/* 3 - Escreva uma query para exibir a soma dos números 10 e 15.*/
 SELECT 10 + 15;
 
-/* 4 - Escreva uma query para exibir o resultado de uma expressão aritmética qualquer. */
+/* 4 - Escreva uma query para exibir o resultado de uma expressão aritmética qualquer.*/
 SELECT 10 * 5 / 2;
 
-/* 5 - Escreva uma query para exibir todas as informações de todos os cientistas. */
+/* 5 - Escreva uma query para exibir todas as informações de todos os cientistas.*/
 SELECT * FROM Scientists;
 
-/* 6 - Escreva uma query para exibir o nome como "nome_do_projeto" e as horas como "tempo_de_trabalho" de cada projeto. */
+/* 6 - Escreva uma query para exibir o nome como "nome_do_projeto" e as horas como "tempo_de_trabalho" de cada projeto.*/
 SELECT name AS nome_do_projeto, hours AS tempo_de_trabalho FROM Projects;
 
-/* 7 - Escreva uma query para exibir o nome dos cientistas em ordem alfabética. */
+/* 7 - Escreva uma query para exibir o nome dos cientistas em ordem alfabética.*/
 SELECT * FROM Scientists
 ORDER BY name;
+
+/* 8 - Escreva uma query para exibir o nome dos projetos em ordem alfabética descendente.*/
+SELECT * FROM Projects
+ORDER BY name DESC;
