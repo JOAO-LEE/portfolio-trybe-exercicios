1b838b28285c378ae5f77cef2a50e1e4fe2dab81
João Lee <joaumlimaum@gmail.com>
2022-09-09 14:59:15 -0300

    Atividade 2

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-1.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-1.sql
@@ -1,3 +1,7 @@
 /*1 - Vamos lá! Faça uma consulta que retorne todas as peças que começam com as letras GR.*/
 SELECT * FROM Pecas
 WHERE name LIKE 'GR%';
+/*2 - Agora, escreva uma query para mostrar todos os fornecimentos que contenham a peça com code 2. Organize o resultado por ordem alfabética de fornecedor.*/
+SELECT * FROM Fornecimentos
+WHERE peca = 2
+ORDER BY fornecedor;
