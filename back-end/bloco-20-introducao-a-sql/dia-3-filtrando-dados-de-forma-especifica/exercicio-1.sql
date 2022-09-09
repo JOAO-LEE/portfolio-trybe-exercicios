f3f2f02e669a601a2474a3d43a891f593c354630
João Lee <joaumlimaum@gmail.com>
2022-09-09 15:21:06 -0300

    Atividade 3

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-1.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-1.sql
@@ -5,3 +5,6 @@ WHERE name LIKE 'GR%';
 SELECT * FROM Fornecimentos
 WHERE peca = 2
 ORDER BY fornecedor;
+/*3 - Em seguida, faça uma consulta para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N.*/
+SELECT peca, fornecedor, Preco FROM Fornecimentos
+WHERE fornecedor LIKE '%N%';
