0942169fd5b58e26424b060e207a09237c06f013
João Lee <joaumlimaum@gmail.com>
2022-09-09 16:29:44 -0300

    Atividade 3

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-bonus.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-bonus.sql
@@ -5,3 +5,7 @@ WHERE name LIKE '%e%';
 SELECT * FROM Projects
 WHERE name LIKE 'a%'
 ORDER BY name;
+/*3 - Escreva uma query para exibir o código e nome de todos os projetos que possuam em seu código o número 3. Ordene o resultado em ordem alfabética.*/
+SELECT code, name FROM Projects
+WHERE code LIKE '%3%'
+ORDER BY name;
