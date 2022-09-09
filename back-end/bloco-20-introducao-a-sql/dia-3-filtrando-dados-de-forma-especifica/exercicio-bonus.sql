0876748cf85acc58de2b9caf3e7cba69da8201ac
João Lee <joaumlimaum@gmail.com>
2022-09-09 16:22:47 -0300

    Atividade 2

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-bonus.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-bonus.sql
@@ -1,3 +1,7 @@
 /*1 - Escreva uma query para exibir todas as informações de todos os cientistas que possuam a letra e em seu nome.*/
 SELECT * FROM Scientists
 WHERE name LIKE '%e%';
+/*2 - Escreva uma query para exibir o nome de todos os projetos cujo o código inicie com a letra A. Ordene o resultado em ordem alfabética.*/
+SELECT * FROM Projects
+WHERE name LIKE 'a%'
+ORDER BY name;
