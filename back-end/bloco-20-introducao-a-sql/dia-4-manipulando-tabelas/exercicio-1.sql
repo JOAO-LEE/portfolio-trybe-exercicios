5236df50c3b66643c0e9adecbf2456e39852ae20
João Lee <joaumlimaum@gmail.com>
2022-09-13 00:53:30 -0300

    Atividade 6

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercicio-1.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercicio-1.sql
@@ -29,3 +29,10 @@ Os Incríveis, classificado em 7.4, lucrou 460 milhões no mercado interno e 510
 WALL-E, classificado em 9.9, lucrou 290 milhões no mercado interno e 280 milhões no mercado internacional.*/
 INSERT INTO BoxOffice (movie_id, rating, domestic_sales, international_sales)
 VALUES (8, 8.5, 300000000, 250000000), (10, 7.4, 460000000, 510000000), (11, 9.9, 290000000, 280000000);
+
+/*Exercício 6: Exclua da tabela Movies o filme "WALL-E".*/
+DELETE FROM BoxOffice
+WHERE movie_id = 11;
+
+DELETE FROM Movies
+WHERE id = 11;
