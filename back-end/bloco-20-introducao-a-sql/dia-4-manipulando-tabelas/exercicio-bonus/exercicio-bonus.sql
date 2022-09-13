ae98737a6977fcbddf67ae26a8bb555de4d05e49
João Lee <joaumlimaum@gmail.com>
2022-09-13 02:15:38 -0300

    Bonus - atividade 2

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercicio-bonus/exercicio-bonus.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercicio-bonus/exercicio-bonus.sql
@@ -2,3 +2,9 @@
 UPDATE BoxOffice
 SET rating = 9.0
 WHERE domestic_sales > 400000000;
+
+/*Exercício 9: Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões no mercado interno.*/
+UPDATE BoxOffice
+SET rating = 6.0
+WHERE international_sales < 300000000
+AND domestic_sales > 200000000;
