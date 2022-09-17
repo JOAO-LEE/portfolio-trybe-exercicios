ed11be9b11f3158945221060d6b6d1d0fdca7900
João Lee <joaumlimaum@gmail.com>
2022-09-16 21:16:17 -0300

    Atividade 1

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercicios-aula/exercicio-aula.sql
@@ -0,0 +1,8 @@
+-- Busque todos os nomes das pessoas que possuem pedidos
+-- realizado e as pessoas funcionárias que fizeram a operação
+SELECT
+CustomerName AS `cliente`, 
+CONCAT(e.FirstName, ' ', e.LastName) AS `funcionario_resp`
+FROM `store-sample`.customers AS c
+INNER JOIN orders ON c.CustomerID = orders.CustomerID
+INNER JOIN employees AS e ON orders.EmployeeID = e.EmployeeID;
