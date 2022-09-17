c3d9253d15e2af55cf1beed7052ca82ab4f50ef5
João Lee <joaumlimaum@gmail.com>
2022-09-16 21:16:43 -0300

    Atividade 2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercicios-aula/exercicio-aula.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercicios-aula/exercicio-aula.sql
@@ -6,3 +6,15 @@ CONCAT(e.FirstName, ' ', e.LastName) AS `funcionario_resp`
 FROM `store-sample`.customers AS c
 INNER JOIN orders ON c.CustomerID = orders.CustomerID
 INNER JOIN employees AS e ON orders.EmployeeID = e.EmployeeID;
+
+-- Busque todos os nomes das pessoas que possuem pedidos realizados e
+-- as pessoas funcionárias que fizeram a operação e a empresa associada
+-- para envio do produto
+SELECT 
+CustomerName AS `cliente`, 
+CONCAT(e.FirstName, ' ', e.LastName) AS `funcionario_resp`,
+sh.ShipperName AS shipper 
+FROM `store-sample`.customers AS c
+INNER JOIN orders ON c.CustomerID = orders.CustomerID
+INNER JOIN employees AS e ON orders.EmployeeID = e.EmployeeID
+INNER JOIN shippers AS sh ON sh.ShipperID = orders.ShipperID
