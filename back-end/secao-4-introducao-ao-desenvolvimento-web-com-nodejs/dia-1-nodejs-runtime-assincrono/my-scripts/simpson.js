340c84b6bbe2e7c3e1914d952a21035d6bf75e1e
João Lee <joaumlimaum@gmail.com>
2022-10-04 18:21:01 -0300

    Simpson por id

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/simpson.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/simpson.js
@@ -3,13 +3,22 @@ const fs = require('fs').promises;
 async function getAllSimpsons(){
   const simpsons = await fs.readFile('./simpsons.json', 'utf-8');
   const convertedSimpsons = await JSON.parse(simpsons);
-  const chosenSimpson = await convertedSimpsons.map(({id, name}) => {
+  const allSimpsons = await convertedSimpsons.map(({id, name}) => {
     console.log(`${id} - ${name}`)
   })
-  return chosenSimpson
+  return allSimpsons
 }
 
+async function getSimpsonById(simpson){
+  const simpsons = await fs.readFile('./simpsons.json', 'utf-8');
+  const convertedSimpsons = await JSON.parse(simpsons);
+  const chosenSimpson = await convertedSimpsons.find((id) => Number(id.id) === simpson);
+  return chosenSimpson
+}
 async function main() {
-  await getAllSimpsons(2);
+  // await getAllSimpsons();
+  const simpson = await getSimpsonById(2);
+  const { id, name } = await simpson; 
+  console.log(`${id} - ${name}`)
 }
 main();
