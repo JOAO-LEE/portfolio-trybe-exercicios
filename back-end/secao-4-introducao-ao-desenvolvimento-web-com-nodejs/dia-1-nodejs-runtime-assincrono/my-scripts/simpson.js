49d6622adb8319650975aee18b011e18b790c88c
João Lee <joaumlimaum@gmail.com>
2022-10-04 17:24:27 -0300

    Atividade 2

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/simpson.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/simpson.js
@@ -2,6 +2,10 @@ const fs = require('fs').promises;
 
 async function getAllSimpsons(){
   const simpsons = await fs.readFile('./simpsons.json', 'utf-8');
+  const convertedSimpsons = JSON.parse(simpsons);
+  convertedSimpsons.map(({id, name}) => {
+    console.log(`${id} - ${name}`)
+  })
 }
 async function main() {
   await getAllSimpsons();
