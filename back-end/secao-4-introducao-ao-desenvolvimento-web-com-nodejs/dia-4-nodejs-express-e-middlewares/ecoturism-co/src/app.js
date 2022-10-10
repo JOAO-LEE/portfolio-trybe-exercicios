0c882fe7de9b7ff69b5613fa6b6f6ce59d6698df
João Lee <joaumlimaum@gmail.com>
2022-10-10 16:21:15 -0300

    Atividade 3 concluída - cadastro e retorno de atividades feito

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/app.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/activities', (req, resp) => {
+  console.log(req.body);
+  resp.status(201).json({ message: "Atividade cadastrada com sucesso!" })
+});
+
+app.get('/activities', (req, resp) => {
+  const activities = req.body;
+  resp.status(200).json(activities)
+})
+
 module.exports = {
   app
 };
