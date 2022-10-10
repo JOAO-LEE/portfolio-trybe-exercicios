f5ff36f6ccf3650b7ac571c08bf33edfc1ddf665
João Lee <joaumlimaum@gmail.com>
2022-10-10 18:36:33 -0300

    Atividade 7 concluída - só aceito formato data dd/mm/yyyy em created at

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { validateName } = require('./middlewares/nameValidation');
 const { priceValidation } = require('./middlewares/priceValidation');
-const { descriptionValidation } = require('./middlewares/descriptionValidation');
+const { descriptionValidation, validFormatDate } = require('./middlewares/descriptionValidation');
 
 const app = express();
 
 app.use(express.json());
 
-app.use(priceValidation, validateName, descriptionValidation);
+app.use(priceValidation, validateName, descriptionValidation, validFormatDate);
 
 app.post('/activities', (_req, resp) => {
   resp.status(201).json({ message: "Atividade cadastrada com sucesso!" })
--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/middlewares/descriptionValidation.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/middlewares/descriptionValidation.js
@@ -2,24 +2,51 @@ const descriptionValidation = (req, resp, next) => {
   const { description } = req.body;
 
   if (!description) {
-    return resp.status(400).json({ "message": "O campo description é obrigatório" })
+    return resp.status(400).json({ "message": "O campo description é obrigatório" });
   }
 
   const { createdAt, rating, difficulty } = description;
 
   if (!createdAt) {
-    return resp.status(400).json({ "message": "O campo createdAt é obrigatório" })
+    return resp.status(400).json({ "message": "O campo createdAt é obrigatório" });
   }
   if (!rating) {
-    return resp.status(400).json({ "message": "O campo rating é obrigatório" })
+    return resp.status(400).json({ "message": "O campo rating é obrigatório" });
   }
   if (!difficulty) {
-    return resp.status(400).json({ "message": "O campo difficulty é obrigatório" })
+    return resp.status(400).json({ "message": "O campo difficulty é obrigatório" });
   }
 
   next();
-}
+};
+
+const validFormatDate = (req, resp, next) => {
+  const { description: { createdAt } } = req.body;
+
+  const validFormat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+
+  if (!validFormat.test(createdAt)) {
+    return resp.status(400).json({ "message": "O campo createdAt deve ter o formato \'dd/mm/aaaa\'" });
+  };
+
+  next();
+};
+
+// const validateRating = (req, resp, next) => {
+//   const { description: { rating } } = req.body
+
+//   const validRating = (rating >= 1 && rating <= 5);
+//   if (!validRating) {
+//     resp.status(400).json({ "message": "O campo rating deve ser um número inteiro entre 1 e 5" });
+//   };
+
+//   next();
+
+// };
+
 
 module.exports = {
-  descriptionValidation
-}
+  descriptionValidation,
+  validFormatDate,
+  // validateRating
+};
