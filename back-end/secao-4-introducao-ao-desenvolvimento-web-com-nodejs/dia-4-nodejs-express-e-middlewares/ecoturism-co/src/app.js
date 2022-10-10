03ca0e836499873256b5b9b90aaf0bb20bf50a6a
João Lee <joaumlimaum@gmail.com>
2022-10-10 20:36:43 -0300

    Stashing changes

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-nodejs-express-e-middlewares/ecoturism-co/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { validateName } = require('./middlewares/nameValidation');
 const { priceValidation } = require('./middlewares/priceValidation');
 const { descriptionValidation, validFormatDate, validateRating, validateDifficulty } = require('./middlewares/descriptionValidation');
+const { registryMiddleware } = require('./middlewares/peopleRegistryMiddleware');
 
 const app = express();
 
@@ -9,14 +10,28 @@ app.use(express.json());
 
 app.use(priceValidation, validateName, descriptionValidation, validFormatDate, validateRating, validateDifficulty);
 
+app.get('/signup', registryMiddleware, (req, resp) => {
+  const accountInfo = req.body
+  console.log(accountInfo)
+  resp.status(200).json(accountInfo);
+});
+
+app.post('/signup', registryMiddleware, (_req, resp) => {
+  resp.status(201).json({ message: "Atividade cadastrada com sucesso!" })
+});
+
 app.post('/activities', (_req, resp) => {
   resp.status(201).json({ message: "Atividade cadastrada com sucesso!" })
 });
 
 app.get('/activities', (req, resp) => {
   const activities = req.body;
+  console.log(activities);
   resp.status(200).json(activities)
 });
+
+
+
 
 module.exports = {
   app
