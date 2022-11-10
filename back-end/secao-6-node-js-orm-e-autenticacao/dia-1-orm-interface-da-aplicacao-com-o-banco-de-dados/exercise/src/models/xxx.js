888627f2a349f1c91db50ccb08a85b9013c0941a
João Lee <joaumlimaum@gmail.com>
2022-11-10 20:59:24 -0300

    Exercício 1

---
+++ back-end/secao-6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise/src/models/xxx.js
@@ -0,0 +1,16 @@
+// const BooksSchema = (sequelize, DataTypes) => {
+//   const BooksTabel = sequelize.define('Book', {
+//     id: DataTypes.INTEGER,
+//     title: DataTypes.STRING,
+//     author: DataTypes.STRING,
+//     pageQuantity: DataTypes.INTEGER,
+//     createdAt: DataTypes.DATE,
+//     updatedAt: DataTypes.DATE
+//   },
+//     {
+//       tableName: 'books',
+//       underscored: true
+//     });
+//   return BooksTabel;
+// };
+// module.exports = BooksSchema;
