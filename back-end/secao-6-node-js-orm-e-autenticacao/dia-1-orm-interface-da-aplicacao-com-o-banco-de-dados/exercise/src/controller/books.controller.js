ee977eb71270938bc9a8e79e6bd7637695a1cc45
João Lee <joaumlimaum@gmail.com>
2022-11-17 17:53:33 -0300

    Conserta alguns códigos de status, adiciona condicional na atividade 17

--- back-end/secao-6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise/src/controller/books.controller.js
+++ back-end/secao-6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercise/src/controller/books.controller.js
@@ -16,7 +16,7 @@ const getById = async (req, resp) => {
 const insertBook = async (req, resp) => {
   const bookToCreate = req.body;
   const insertedBook = await bookService.insertBook(bookToCreate);
-  return resp.status(200).json(insertedBook.message);
+  return resp.status(201).json(insertedBook.message);
 };
 
 const updateBook = async (req, resp) => {
@@ -29,8 +29,9 @@ const updateBook = async (req, resp) => {
 
 const deletedBook = async (req, resp) => {
   const { id } = req.params;
-  await bookService.deleteBook(id);
-  return resp.status(200).json({ message: "deletado" })
+  const deletedBook = await bookService.deleteBook(id);
+  if (!deletedBook) return resp.status(500).json({message: "Algo deu errado..."})
+  return resp.status(204).json()
 };
 
 
