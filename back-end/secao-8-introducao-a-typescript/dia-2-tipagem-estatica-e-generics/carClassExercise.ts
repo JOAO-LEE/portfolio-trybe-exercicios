29875e4fe638a7a716f8a7fa20eefac4b0e3dcc4
João Lee <joaumlimaum@gmail.com>
2023-02-07 17:48:39 -0300

    Exercício 2 concluído

--- back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/carClassExercise.ts
+++ back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/carClassExercise.ts
@@ -9,4 +9,32 @@ class Car {
     this.color = color;
     this.doors = doors;
   }
+
+  honk(): void {
+    console.log("Bibi!");
+  }
+  
+  turnOn(): void {
+    console.log('Carro ligado!')
+  }
+
+  turnOff(): void {
+    console.log('Carro desligado!');
+  }
+
+  speedUp(velocity: string): void {
+    console.log(`Velocidade aumentada para ${velocity}`);
+  }
+
+  speedDown(velocity: string): void {
+    console.log(`Velocidade reduzida para ${velocity}`);
+  }
+
+  stop(): void {
+    console.log('Carro parado!');
+  }
+
+  turn(direction: string): void {
+    console.log(`Carro virando para ${direction}`);
+  }
 }
