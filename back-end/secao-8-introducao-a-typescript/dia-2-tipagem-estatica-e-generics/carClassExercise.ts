af934a57bc0a5d766e015765a26a0a3015383770
João Lee <joaumlimaum@gmail.com>
2023-02-07 17:43:45 -0300

    Exercício 1 concluído

---
+++ back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/carClassExercise.ts
@@ -0,0 +1,12 @@
+class Car {
+
+  brand: string;
+  color: string;
+  doors: number;
+
+  constructor(brand: string, color: string, doors: number) {
+    this.brand = brand;
+    this.color = color;
+    this.doors = doors;
+  }
+}
