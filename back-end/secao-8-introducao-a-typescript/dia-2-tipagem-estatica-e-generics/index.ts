d6b678ca1a009b3c8e6d8a7ebe0f814470150ff8
João Lee <joaumlimaum@gmail.com>
2023-02-07 18:22:37 -0300

    Continuação 2 do exercício 2

--- back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/index.ts
+++ back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/index.ts
@@ -12,3 +12,16 @@ newCar.speedDown('25');
 newCar.turn('direita');
 newCar.speedUp('50');
 newCar.speedDown('0');
+//passenger gets into the car.
+newCar.speedUp('50');
+newCar.speedDown('22');
+newCar.turn('direita');
+newCar.speedUp('75');
+newCar.speedDown('10');
+newCar.turn('esquerda');
+newCar.speedUp('43');
+newCar.speedDown('22');
+newCar.turn('direita');
+newCar.speedUp('64');
+newCar.speedDown('0');
+newCar.turnOff();
