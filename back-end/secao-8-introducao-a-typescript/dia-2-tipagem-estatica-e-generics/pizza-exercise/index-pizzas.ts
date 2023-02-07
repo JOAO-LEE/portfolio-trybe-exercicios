04f7cf2996f00d48a192785582364d1b24d7ce00
João Lee <joaumlimaum@gmail.com>
2023-02-07 18:51:12 -0300

    Segunda parte do exercício 3 concluída

---
+++ back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/pizza-exercise/index-pizzas.ts
@@ -0,0 +1,16 @@
+import {IPizza} from './interface';
+
+const pepperoni: IPizza = {
+  flavor: 'Calabresa',
+  slices: 8,
+};
+
+const marguerita: IPizza = {
+  flavor: 'Marguerita',
+  slices: 6,
+};
+
+const nutella: IPizza = {
+  flavor: 'Nutella',
+  slices: 4,
+}
