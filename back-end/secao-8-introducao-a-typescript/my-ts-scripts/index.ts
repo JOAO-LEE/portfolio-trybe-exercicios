68e3365c55daf50b8d1db0415f9ed7012994cdfd
João Lee <joaumlimaum@gmail.com>
2023-02-06 22:40:38 -0300

    Exercício 3 concluído

--- back-end/secao-8-introducao-a-typescript/my-ts-scripts/index.ts
+++ back-end/secao-8-introducao-a-typescript/my-ts-scripts/index.ts
@@ -22,3 +22,11 @@ const validateEmailFormat = (param: string) => {
 };
 
 console.log(validateEmailFormat(emailInput))
+
+const numberList = [10, 5, 18, 2, 8, 23];
+
+const sortInput = (param: number[]) => {
+  return param.sort(function(a, b){return a-b});
+};
+
+console.log(sortInput(numberList));
