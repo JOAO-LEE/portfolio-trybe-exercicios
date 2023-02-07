0d3c346e64c62aae74dbe056b878e7dad07a18a1
João Lee <joaumlimaum@gmail.com>
2023-02-07 19:22:01 -0300

    Exercício 4 concluído

--- back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/pizza-exercise/index-pizzas.ts
+++ back-end/secao-8-introducao-a-typescript/dia-2-tipagem-estatica-e-generics/pizza-exercise/index-pizzas.ts
@@ -1,6 +1,6 @@
-import {IPizza} from './interface';
+import {IPizza, CommonFlavors, vegatarianFlavors, sweetFlavors} from './interface';
 
-const pepperoni: IPizza = {
+const calabresa: IPizza = {
   flavor: 'Calabresa',
   slices: 8,
 };
@@ -13,4 +13,39 @@ const marguerita: IPizza = {
 const nutella: IPizza = {
   flavor: 'Nutella',
   slices: 4,
-}
+};
+
+//EXTENDED FLAVORS:
+
+//Common flavors:
+const calabresa2: CommonFlavors = {
+  flavor: 'Calabresa',
+  slices: 6,
+};
+
+const chicken: CommonFlavors = {
+  flavor: 'Frango',
+  slices: 4,
+};
+
+const pepperoni: CommonFlavors = {
+  flavor: 'Pepperoni',
+  slices: 8,
+};
+
+//Vegeterian flavors:
+const palmito: vegatarianFlavors = {
+  flavor: 'Palmito',
+  slices: 8
+};
+
+const cogumelo: vegatarianFlavors = {
+  flavor: 'Cogumelos',
+  slices: 4,
+};
+
+//Sweet flavors:
+const marshmellow: sweetFlavors = {
+  flavor: 'Marshmallow',
+  slices: 4,
+};
