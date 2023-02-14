7e057c3d7823baf6f5920e4845e6f5b385d63d3a
João Lee <joaumlimaum@gmail.com>
2023-02-14 12:15:38 -0300

    Exercício 2 concluído

--- back-end/secao-9-programacao-orientada-a-objetos-poo-e-solid/dia-1-introducao-a-orientaçao-a-objetos/index.ts
+++ back-end/secao-9-programacao-orientada-a-objetos-poo-e-solid/dia-1-introducao-a-orientaçao-a-objetos/index.ts
@@ -3,4 +3,11 @@ export default class PessoaEstudante {
   private nome: string;
   private notasTeste: number[];
   private notasTrabalho: number[];
+
+  constructor(matricula: number, nome: string , notasTeste: number[], notasTrabalho: number[]) {
+    this.matricula = matricula;
+    this.nome = nome;
+    this.notasTeste = notasTeste;
+    this.notasTrabalho = notasTrabalho;
+  }
 }
